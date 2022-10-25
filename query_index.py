87b7b5a1d34b3dc4f1f78d289cfd1eeb8e269b3a
bentrevett <bentrevett@gmail.com>
2022-10-25 23:14:46 +0100

    added file for querying index

---
+++ query_index.py
@@ -0,0 +1,18 @@
+import sentence_transformers
+import datasets
+import faiss
+
+query = "What is AGI?"
+
+model = sentence_transformers.SentenceTransformer("sentence-transformers/multi-qa-mpnet-base-dot-v1", device="cuda")
+
+dataset = datasets.load_dataset("json", data_files=["dataset.jsonl"], split="train")
+dataset.load_faiss_index("embeddings", "index.faiss")
+
+query_embedding = model.encode(query)
+scores, retrieved_examples = dataset.get_nearest_examples("embeddings", query_embedding, k=10)
+
+for score, example in zip(scores, retrieved_examples["text"]):
+    print(score)
+    print(example)
+    print("*" * 100)
