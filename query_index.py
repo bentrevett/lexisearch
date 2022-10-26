2d27c1a18dc1637b1f9de3e6ee89690492cb1a2c
bentrevett <bentrevett@gmail.com>
2022-10-26 19:57:24 +0100

    add query arg

--- query_index.py
+++ query_index.py
@@ -1,7 +1,11 @@
+import argparse
+
 import datasets
 import sentence_transformers
 
-query = "What is AGI?"
+parser = argparse.ArgumentParser()
+parser.add_argument("--query", type=str, required=True)
+args = parser.parse_args()
 
 model = sentence_transformers.SentenceTransformer(
     "sentence-transformers/multi-qa-mpnet-base-dot-v1", device="cuda"
@@ -10,7 +14,7 @@ model = sentence_transformers.SentenceTransformer(
 dataset = datasets.load_dataset("json", data_files=["dataset.jsonl"], split="train")
 dataset.load_faiss_index("embeddings", "index.faiss")
 
-query_embedding = model.encode(query)
+query_embedding = model.encode(args.query)
 scores, retrieved_examples = dataset.get_nearest_examples(
     "embeddings", query_embedding, k=10
 )
